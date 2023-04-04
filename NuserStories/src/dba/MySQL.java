f5177d263017675f48f9f63694547f8dc0f7f738
Nahuel <nahueltorrezg@gmail.com>
2023-04-04 09:44:56 -0400

    Subido por Nahuel Torrez

--- NuserStories/src/dba/MySQL.java
+++ NuserStories/src/dba/MySQL.java
@@ -2,15 +2,14 @@
 package dba;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
         
 public class MySQL {
-    public static String url = "jdbc:mysql://localhost/sisInfo";
+    public static String url = "jdbc:mysql://127.0.0.1/sisinformacion";
     public static String usuario = "root";
     public static String contraseña = "";
-    public static String clase = "com.mysql.jdbc.Driver";
+    public static String clase = "com.mysql.cj.jdbc.Driver";
     
     public static Connection getConnection(){
          Connection conexion = null;
@@ -18,11 +17,16 @@ public class MySQL {
             Class.forName(clase);
             conexion = DriverManager.getConnection(url,usuario,contraseña);
             System.out.println("Conexion establecida");
-        }catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-        }catch(Exception e1){
-            System.out.println("Error: " + e1.getMessage());
+        }catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Falla en la conexion: " + e.getMessage());
         }
     return  conexion;
     }
+    
+    public static void main(String[] args) {
+        MySQL.getConnection();
+    }
+
+    
 }
+
