8d1de87ff37340f7c1b5e271a9ea6285443e4a4a
Joseph Alejandro Vargas Tordoya <Josephvargastordoya@gmail.com>
2023-04-05 03:35:10 -0400

    a.v

---
+++ NuserStories/src/sisinfo/historia.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sisinfo;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class historia {
+    private String rol;
+    private String como;
+    private String motivo;
+    private String ca;
+    
+}
