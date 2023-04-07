b30dd1ee1d11000d45d57805c4f38fbbad4ac2be
Joseph Alejandro Vargas Tordoya <Josephvargastordoya@gmail.com>
2023-04-07 05:29:37 -0400

    guardar

--- NuserStories/src/IU/Begin.java
+++ NuserStories/src/IU/Begin.java
@@ -5,11 +5,16 @@
 package IU;
 
 import dba.MySQL;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Set;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 
@@ -19,6 +24,11 @@ public class Begin extends javax.swing.JFrame {
     private String user;
     private int palabras;
     private boolean correcto;
+    
+    JFileChooser seleccionar = new JFileChooser();
+        File historia;
+        FileInputStream entrada;
+        FileOutputStream salida;
     
     public Begin() {
         initComponents();
@@ -35,100 +45,101 @@ public class Begin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
-        jToggleButton1 = new javax.swing.JToggleButton();
-        jToggleButton2 = new javax.swing.JToggleButton();
+        mostrar = new javax.swing.JToggleButton();
+        guardar = new javax.swing.JToggleButton();
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
+        lista = new javax.swing.JButton();
+        yo = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea2 = new javax.swing.JTextArea();
+        requiero = new javax.swing.JTextArea();
         jLabel3 = new javax.swing.JLabel();
         jScrollPane3 = new javax.swing.JScrollPane();
-        jTextArea3 = new javax.swing.JTextArea();
+        talque = new javax.swing.JTextArea();
         jLabel4 = new javax.swing.JLabel();
         jScrollPane4 = new javax.swing.JScrollPane();
-        jTextArea4 = new javax.swing.JTextArea();
-        jButton2 = new javax.swing.JButton();
+        ca = new javax.swing.JTextArea();
+        jScrollPane5 = new javax.swing.JScrollPane();
+        docs = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
-
-        jToggleButton1.setText("Mostrar");
-        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
+        mostrar.setText("Mostrar");
+        mostrar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jToggleButton1ActionPerformed(evt);
+                mostrarActionPerformed(evt);
             }
         });
 
-        jToggleButton2.setText("Guardar");
+        guardar.setText("Guardar");
+        guardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                guardarActionPerformed(evt);
+            }
+        });
 
         jLabel1.setText("Yo como");
 
-        jButton1.setText("Lista de User Stories");
-
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        lista.setText("Lista de User Stories");
+        lista.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                listaActionPerformed(evt);
+            }
+        });
+
+        yo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                yoActionPerformed(evt);
             }
         });
 
         jLabel2.setText("requiero");
 
-        jTextArea2.setColumns(20);
-        jTextArea2.setRows(5);
-        jScrollPane2.setViewportView(jTextArea2);
+        requiero.setColumns(20);
+        requiero.setRows(5);
+        jScrollPane2.setViewportView(requiero);
 
         jLabel3.setText("tal que");
 
-        jTextArea3.setColumns(20);
-        jTextArea3.setRows(5);
-        jScrollPane3.setViewportView(jTextArea3);
+        talque.setColumns(20);
+        talque.setRows(5);
+        jScrollPane3.setViewportView(talque);
 
         jLabel4.setText("Condiciones de Utilizacion");
 
-        jTextArea4.setColumns(20);
-        jTextArea4.setRows(5);
-        jScrollPane4.setViewportView(jTextArea4);
+        ca.setColumns(20);
+        ca.setRows(5);
+        jScrollPane4.setViewportView(ca);
 
-        jButton2.setText("jButton2");
+        docs.setColumns(20);
+        docs.setRows(5);
+        jScrollPane5.setViewportView(docs);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1082, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(30, 30, 30)
-                .addComponent(jButton2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 71, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGap(188, 188, 188)
+                .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, 964, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 314, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(yo, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4)
+                    .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel3)
-                            .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel4)
-                            .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(202, 202, 202))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jButton1)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(jToggleButton2)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jToggleButton1))))
-                        .addGap(45, 45, 45))))
+                        .addComponent(guardar)
+                        .addGap(87, 87, 87)
+                        .addComponent(mostrar))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(55, 55, 55)
+                        .addComponent(lista)))
+                .addGap(43, 43, 43))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -138,20 +149,11 @@ public class Begin extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jToggleButton1))
-                        .addGap(43, 43, 43)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel2)
-                            .addComponent(jToggleButton2))
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(68, 68, 68)
-                                .addComponent(jButton2)))
+                        .addComponent(yo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(47, 47, 47)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(43, 43, 43)
                         .addComponent(jLabel3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -160,24 +162,39 @@ public class Begin extends javax.swing.JFrame {
                         .addComponent(jLabel4)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 141, Short.MAX_VALUE))
-                    .addComponent(jScrollPane1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
-                .addGap(87, 87, 87))
+                        .addGap(73, 73, 73)
+                        .addComponent(lista))
+                    .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, 754, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(39, 39, 39)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(guardar)
+                    .addComponent(mostrar))
+                .addGap(0, 97, Short.MAX_VALUE))
         );
 
-        jTextField1.getAccessibleContext().setAccessibleName("");
+        yo.getAccessibleContext().setAccessibleName("");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void yoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_yoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_yoActionPerformed
 
-    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
-        //boolean result = metodos.buscarUsuario(txtUser.getText(), txtPassword.getText());
+    private void mostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mostrarActionPerformed
+     if(yo.getText().isEmpty() || requiero.getText().isEmpty() || talque.getText().isEmpty() || ca.getText().isEmpty()){
+     
+         JOptionPane.showMessageDialog(null, "DEBES LLENAR TODOS LOS CAMPOS");
+     }else{
+                String x = "Yo Como " + yo.getText() + '\n';
+                String y = "Requiero " + requiero.getText() + '\n';
+                String z = "tal que  " + talque.getText() + '\n';
+                 String m = "Condiciones de aceptación: " + '\n' + ca.getText();
+    docs.setText(x+ '\n'+ y +'\n' + z + '\n' + m);
+     }
+        
+
+//boolean result = metodos.buscarUsuario(txtUser.getText(), txtPassword.getText());
     //if(result == true){
         /*Connection conexion = TBD.getConnection();
         String consulta = "SELECT pg_backend_pid();";
@@ -194,7 +211,75 @@ public class Begin extends javax.swing.JFrame {
     }else{
         JOptionPane.showMessageDialog(this, "No esta registrado");
     }*/
-    }//GEN-LAST:event_jToggleButton1ActionPerformed
+    }//GEN-LAST:event_mostrarActionPerformed
+   
+       
+        public String abrirArchivo(File archivo){
+            String doc= "";
+        
+        try{
+        entrada = new FileInputStream(historia);
+        int ascci;
+        while( (ascci = entrada.read()) != -1){
+            char caracter = (char) ascci;
+            doc+=caracter;
+        }
+        
+    } catch(Exception e){
+    
+    }
+        return doc;
+        
+        } 
+    
+    public String guardarArchivo(File archivo,String documento ){
+        String mensaje = null;
+        try {
+            salida = new FileOutputStream(archivo);
+            byte[] bytxt = documento.getBytes();
+            salida.write(bytxt);
+            mensaje="Archivo guardado ahora puedes compartirlo en la red social de tu preferencia";
+        } catch(Exception e){
+        }
+        return mensaje;
+    }
+        
+        
+        
+    private void guardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_guardarActionPerformed
+        
+        if(yo.getText().isEmpty() || requiero.getText().isEmpty() || talque.getText().isEmpty() || ca.getText().isEmpty()){
+     
+         JOptionPane.showMessageDialog(null, "DEBES LLENAR TODOS LOS CAMPOS");
+     }else{
+             if(seleccionar.showDialog(null, "Guardar") == JFileChooser.APPROVE_OPTION){
+            historia=seleccionar.getSelectedFile();
+            if(historia.getName().endsWith("txt")){
+                String doc = docs.getText();
+                String cont = guardarArchivo(historia, doc);
+                if(cont != null){
+                    JOptionPane.showMessageDialog(null, cont);
+                }else{
+                    JOptionPane.showMessageDialog(null, "archivo no compatible");
+                }
+            
+                
+                docs.setText(doc);
+            }else{
+                JOptionPane.showMessageDialog(null, "Guardar Documento de texto");
+            }
+        }
+        
+        }
+            
+    }//GEN-LAST:event_guardarActionPerformed
+    
+    private void listaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_listaActionPerformed
+        // TODO add your handling code here://lista
+        Lista newframe= new Lista();
+        newframe.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_listaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -232,22 +317,21 @@ public class Begin extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JTextArea ca;
+    private javax.swing.JTextArea docs;
+    private javax.swing.JToggleButton guardar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JScrollPane jScrollPane4;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextArea jTextArea2;
-    private javax.swing.JTextArea jTextArea3;
-    private javax.swing.JTextArea jTextArea4;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JToggleButton jToggleButton1;
-    private javax.swing.JToggleButton jToggleButton2;
+    private javax.swing.JScrollPane jScrollPane5;
+    private javax.swing.JButton lista;
+    private javax.swing.JToggleButton mostrar;
+    private javax.swing.JTextArea requiero;
+    private javax.swing.JTextArea talque;
+    private javax.swing.JTextField yo;
     // End of variables declaration//GEN-END:variables
 }
